da68c34d0feb40447665b74e6fd8b578244d14ca
Frank Gerold <FrankETSGerold@gmail.com>
2020-08-06 16:06:08 -0400

    Revert "added token column to user model"
    This reverts commit fcfacb06fab36d9cfecf09b818563c882f64b9ca.

--- db/migrate/20200805184141_add_push_token_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPushTokenToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :PushToken, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_184141) do
+ActiveRecord::Schema.define(version: 2020_08_04_184456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2020_08_05_184141) do
     t.string "uu"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "PushToken"
   end
 
 end
