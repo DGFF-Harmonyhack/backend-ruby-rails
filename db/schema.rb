b9a9c3dc08e2a96a340a4c6f956a33aa46987bd8
Frank Gerold <FrankETSGerold@gmail.com>
2020-08-06 16:00:43 -0400

    made lat/long regular decimals for max accuracy.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_185841) do
+ActiveRecord::Schema.define(version: 2020_08_06_195245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 2020_08_05_185841) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.decimal "lat", precision: 10, scale: 6
-    t.decimal "long", precision: 10, scale: 6
+    t.decimal "lat"
+    t.decimal "long"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
