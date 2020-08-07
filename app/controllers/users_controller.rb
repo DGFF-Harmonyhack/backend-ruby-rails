85d9e599af79901e268dec55d351c56a04a1ae21
domitalk <dominic.m.chu@gmail.com>
2020-08-07 09:53:32 -0400

    made user controller REST because separate response aand event calls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,14 @@ class UsersController < ApplicationController
         
         @user = User.create(uu: SecureRandom.hex(10))
 
-        @user_final = {
-            id: @user.id, 
-            uuid: @user.uu, 
-            saved_events: [],
-            saved_responses: [],
-            response_index: 1,
-            event_index: 1
-        }
+        # @user_final = {
+        #     id: @user.id, 
+        #     uuid: @user.uu, 
+        #     saved_events: [],
+        #     saved_responses: [],
+        #     response_index: 1,
+        #     event_index: 1
+        # }
 
         render json: @user_final.to_json 
     end 
@@ -38,36 +38,36 @@ class UsersController < ApplicationController
 
         @user = User.find(params[:id])
         
-        @responses = Response.all.select{|response| response.user_id == @user.id}.reverse()
-        @events = Event.all.select{|event| event.user_id == @user.id}.reverse()
+        # @responses = Response.all.select{|response| response.user_id == @user.id}.reverse()
+        # @events = Event.all.select{|event| event.user_id == @user.id}.reverse()
 
-        @response_page = params[:response_index].to_i
-        @event_page = params[:event_index].to_i
+        # @response_page = params[:response_index].to_i
+        # @event_page = params[:event_index].to_i
 
-        if (@responses.length() > (5 * @response_page))
-            @response_index = ((@response_page * 5) - 4)
-            @saved_responses = @responses.slice(@response_index..5)
-            @response_page +=1
-        else
-            @saved_responses = @responses
-        end 
+        # if (@responses.length() > (5 * @response_page))
+        #     @response_index = ((@response_page * 5) - 4)
+        #     @saved_responses = @responses.slice(@response_index..5)
+        #     @response_page +=1
+        # else
+        #     @saved_responses = @responses
+        # end 
 
-        if (@events.length() > (5 * @event_page))
-            @event_index = ((@event_page * 5) - 4)
-            @saved_events = @events.slice(@response_index..5)
-            @event_page += 1
-        else 
-            @saved_events = @events 
-        end 
+        # if (@events.length() > (5 * @event_page))
+        #     @event_index = ((@event_page * 5) - 4)
+        #     @saved_events = @events.slice(@response_index..5)
+        #     @event_page += 1
+        # else 
+        #     @saved_events = @events 
+        # end 
 
-        @user_info = {
-            user_id: @user.id, 
-            uuid: @user.uu, 
-            saved_events: @saved_events, 
-            saved_responses: @saved_responses, 
-            response_index: @response_page, 
-            event_index: @event_page
-        }
+        # @user_info = {
+        #     user_id: @user.id, 
+        #     uuid: @user.uu, 
+        #     saved_events: @saved_events, 
+        #     saved_responses: @saved_responses, 
+        #     response_index: @response_page, 
+        #     event_index: @event_page
+        # }
         render json: @user_info.to_json
     end 
 
